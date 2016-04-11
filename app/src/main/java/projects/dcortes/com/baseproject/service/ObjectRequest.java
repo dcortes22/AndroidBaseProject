f8100e233c5742d748960dc36f5c95a76380ded4
David Cortes <dcortes22@gmail.com>
2016-04-11 00:43:07 -0600

    Fix issue on send puts or post to the server

--- app/src/main/java/projects/dcortes/com/baseproject/service/ObjectRequest.java
+++ app/src/main/java/projects/dcortes/com/baseproject/service/ObjectRequest.java
@@ -9,6 +9,7 @@ import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonRequest;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -22,7 +23,7 @@ import java.util.Map;
 /**
  * Created by dcortess on 3/15/16.
  */
-public class ObjectRequest<T> extends Request<T> {
+public class ObjectRequest<T> extends JsonRequest<T> {
 
     private final Gson gson = new Gson();
     private final Class clazz;
@@ -33,11 +34,11 @@ public class ObjectRequest<T> extends Request<T> {
     private static final String PROTOCOL_CHARSET="utf-8";
     private static final String TAG="ObjectRequest";
     private Map<String, String> mHeaders;
-    private Map<String, String> mParams;
+    private Map<String, Object> mParams;
 
-    public ObjectRequest(int method, String url, Map<String, String> params, Class clazz,
+    public ObjectRequest(int method, String url, Map<String, Object> params, Class clazz,
                          Response.Listener<T> listener, Response.ErrorListener errorListener) {
-        super(method, url, errorListener);
+        super(method, url, new Gson().toJson(params), listener, errorListener);
         this.clazz = clazz;
         this.listener = listener;
         if(params!=null){
@@ -56,19 +57,21 @@ public class ObjectRequest<T> extends Request<T> {
     public String getUrl() {
         if(mMethod == Request.Method.GET) {
             StringBuilder stringBuilder = new StringBuilder(mUrl);
-            Iterator<Map.Entry<String, String>> iterator = mParams.entrySet().iterator();
-            int i = 1;
-            while (iterator.hasNext()) {
-                Map.Entry<String, String> entry = iterator.next();
-                if(i == 1) {
-                    stringBuilder.append("?" + entry.getKey() + "=" + entry.getValue());
-                } else {
-                    stringBuilder.append("&" + entry.getKey() + "=" + entry.getValue());
+            if (mParams != null) {
+                Iterator<Map.Entry<String, Object>> iterator = mParams.entrySet().iterator();
+                int i = 1;
+                while (iterator.hasNext()) {
+                    Map.Entry<String, Object> entry = iterator.next();
+                    if(i == 1) {
+                        stringBuilder.append("?" + entry.getKey() + "=" + entry.getValue());
+                    } else {
+                        stringBuilder.append("&" + entry.getKey() + "=" + entry.getValue());
+                    }
+                    iterator.remove(); // avoids a ConcurrentModificationException
+                    i++;
                 }
-                iterator.remove(); // avoids a ConcurrentModificationException
-                i++;
+                mUrl = stringBuilder.toString();
             }
-            mUrl = stringBuilder.toString();
         }
         return mUrl;
     }
@@ -81,15 +84,6 @@ public class ObjectRequest<T> extends Request<T> {
             Log.e(TAG, "Unsupported Encoding while trying to get the bytes of " + mRequestBody +
                        "using " + PROTOCOL_CHARSET);
             return null;
-        }
-    }
-
-    @Override
-    protected Map<String, String> getParams() throws AuthFailureError {
-        if (mParams.isEmpty()) {
-            return super.getParams();
-        }else{
-            return mParams;
         }
     }
 
